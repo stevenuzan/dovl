73c650f2a8358f71fbb05b4f1adb1cf1bd85f8b3
stevenuzan <stevenuzan@hotmail.com>
2015-05-30 15:15:10 +0200

    Initial Commit

---
+++ Gitrepo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Gitrepo
+//
+//  Created by Steven Uzan on 30/05/2015.
+//  Copyright (c) 2015 Steven Uzan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
